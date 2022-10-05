7949416b0149840baa7651e11ee07cd537b811ad
yasinyavuz <yavuzyasin.eem@gmail.com>
2022-10-05 14:44:38 +0300

    color changed
    again color changed

--- deneme/lib/main.dart
+++ deneme/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.yellow,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
